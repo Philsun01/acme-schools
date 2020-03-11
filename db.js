58bb445a4b7b5192640ab1ac299967b1de93d94f
Philip Sun <philipsun@Philips-MacBook-Pro.local>
2020-03-10 20:10:49 -0700

    created delete student method

--- db.js
+++ db.js
@@ -29,10 +29,10 @@ const sync = async() => {
     `;
 
     client.query(SQL);
+
     const seedSchools = ['UCLA', 'USC', 'UCSD', 'Berkeley', 'Cal State Fullerton'];
-    const [UCLA, USC, UCSD, BERKELEY, CSUF] = 
-    await Promise.all(seedSchools.map(school=> createSchool(school)));
-    console.log(UCLA);
+    const [UCLA, USC, UCSD, BERKELEY, CSUF] = await Promise.all(seedSchools.map(school=> createSchool(school)));
+    
     const seedStudents = [
         { firstName: 'Mike', lastName: 'Jordan', schoolId: USC.id },
         { firstName: 'Jim', lastName: 'Smith', schoolId: UCSD.id },
@@ -43,25 +43,49 @@ const sync = async() => {
         { firstName: 'Stanley', lastName: 'Marsh', schoolId: UCLA.id },
         { firstName: 'Bart', lastName: 'Simpson', schoolId: UCLA.id },
     ];
-    
-    Promise.all(seedStudents.map(student=>createStudent(student.firstName, student.lastName, student.schoolId)));
+    const [Mike, Jim, Daniel, Jennifer, Lisa, Natalie, Stanley, Bart] = await Promise.all(seedStudents.map(student=>createStudent(student.firstName, student.lastName, student.schoolId)));
+    await Promise.all([ 
+        deleteStudent(Mike.id),
+        deleteStudent(Jim.id),
+        deleteStudent(Daniel.id),
+        deleteStudent(Jennifer.id),
+        deleteStudent(Lisa.id),
+        deleteStudent(Natalie.id),
+        deleteStudent(Stanley.id)
+    ])
+    console.log(await readStudents());
 
 };
 
 const createSchool = async(name) => {
     return (await client.query('INSERT INTO schools(name) VALUES($1) RETURNING *', [name])).rows[0];
-}
+};
 
 const createStudent = async(firstName, lastName, schoolId) => {
     const SQL = 'INSERT INTO students(first_name, last_name, "schoolId") VALUES( $1, $2, $3) RETURNING *';
     return (await client.query(SQL, [firstName, lastName, schoolId])).rows[0];
-}
+};
+
+const readSchools = async() => {
+    return (await client.query('SELECT * FROM schools')).rows;
+};
+
+const readStudents = async() => {
+    return (await client.query('SELECT * FROM students')).rows;
+};
+
+const deleteStudent = async(id) => {
+    return await client.query('DELETE FROM students WHERE id = $1', [id]);
+};
 
 sync();
 
 module.exports = {
     sync,
     createSchool,
-    createStudent
+    createStudent,
+    readSchools,
+    readStudents,
+    deleteStudent
     
 }
