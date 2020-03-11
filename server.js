de49c8f06e7add20217abf98e6d559388fc1555d
Philip Sun <philipsun@Philips-MacBook-Pro.local>
2020-03-10 21:11:37 -0700

    server read api setup

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const db = require('./db');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+
+app.get('/', (req, res, next) => {
+    res.sendFile( path.join(__dirname,'index.html'));
+});
+
+app.get('/api/schools', (req, res, next)=>{
+    db.readSchools()
+        .then( (data)=>res.send(data))
+        .catch(next);
+});
+
+app.get('/api/students', (req, res, next)=>{
+    db.readStudents()
+        .then( (data)=>res.send(data))
+        .catch(next);
+});
+
+app.listen(port, ()=> console.log(`Listening on ${port}`));
