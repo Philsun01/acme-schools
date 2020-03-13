31e1853f8d8b328658c173cc9ddc064b4dc03bdd
Philip Sun <philipsun@Philips-MacBook-Pro.local>
2020-03-12 19:31:14 -0700

    page loads initial data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import SchoolList from './SchoolList';
 import StudentList from './StudentList';
 
 const App = () => {
 
+    const [schools, setSchools] = useState([]);
+    const [students, setStudents] = useState([]);
+
+    useEffect( ()=>{
+        Promise.all([
+          axios.get('./api/schools'),
+          axios.get('./api/students')  
+        ])
+        .then( res => {
+            setSchools(res[0].data);
+            setStudents(res[1].data);
+            console.log(res[1].data);
+            })
+
+    }, []);
+
     return(
         <div>
             <h1> This is the App.js</h1>
+            {schools[0] && <h2>Test School is {schools[0].name}</h2>}
+            {students[0] && <h2>Test Student is {students[0].first_name}</h2>}
+           
             <SchoolList />
             <StudentList />
         </div>
